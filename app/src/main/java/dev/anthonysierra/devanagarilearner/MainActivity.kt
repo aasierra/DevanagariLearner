1dde27d839ec487dc15b0a08c9d96e11b76b20db
Anthony Sierra <aasierra@live.com>
2022-09-12 06:56:14 -0700

    Updated to have the capability to add multiple devanagari letters

--- app/src/main/java/dev/anthonysierra/devanagarilearner/MainActivity.kt
+++ app/src/main/java/dev/anthonysierra/devanagarilearner/MainActivity.kt
@@ -2,10 +2,34 @@ package dev.anthonysierra.devanagarilearner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private data class LetterPossibility(
+        val drawableResourceId: Int,
+        val englishLetterEquiavelent: String,
+    )
+
+    private val letter_possibilities = listOf<LetterPossibility>(
+        LetterPossibility(R.drawable.letter_a, "a")
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        reload()
+    }
+
+    private fun reload() {
+        val position = Random.nextInt(0, letter_possibilities.size)
+        val letter = letter_possibilities[position]
+        findViewById<ImageView>(R.id.letter_image).setImageResource(letter.drawableResourceId)
+        findViewById<TextView>(R.id.english_equivalent).setText("English letter : " + letter.englishLetterEquiavelent)
     }
 }
